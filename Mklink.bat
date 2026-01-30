d3174f6c6ebabf70a648d52623ee32a2466dfd75
tangchunguang_db <394266315@qq.com>
2026-01-30 10:36:19 +0800

    no message

---
+++ Mklink.bat
@@ -0,0 +1,14 @@
+set dir=Unity2
+
+if not exist %dir% ( md %dir%)
+
+mklink/J H:\GitWeiJing\Unity2\Assets H:\GitWeiJing\Unity\Assets
+
+mklink/J H:\GitWeiJing\Unity2\ProjectSettings H:\GitWeiJing\Unity\ProjectSettings
+
+mklink/J H:\GitWeiJing\Unity2\Packages H:\GitWeiJing\Unity\Packages
+
+mklink/J H:\GitWeiJing\Unity2\ProjectSettings H:\GitWeiJing\Unity\ProjectSettings
+
+
+pause
---
+++ start_1_server1.bat
@@ -0,0 +1,3 @@
+cd bin
+dotnet Server.dll --AppType=Server --Console=1 --Title=Gate --StartConfig=StartConfig/Localhost --Process 1
+pause
